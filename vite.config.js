103f4ba7d7da3898f7aacbd5743f5cfd7b520227
1248801340 <1248801340@qq.com>
2026-07-28 12:08:04 +0800

    添加 GitHub Pages 自动部署配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',          // 方便以后部署到子路径
+  base: '/my-game-MortalImmortal/',          // 方便以后部署到子路径
   server: {
     port: 5173,
     open: true         // npm run dev 自动开浏览器
