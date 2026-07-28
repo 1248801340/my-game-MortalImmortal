3b6d48b3f2058cc1ec05042d3f712522f876ce6f
1248801340 <1248801340@qq.com>
2026-07-28 12:49:05 +0800

    改用本地打包 + docs 静态部署，移除云端 build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/my-game-MortalImmortal/',          // 方便以后部署到子路径
+  base: './',          // 方便以后部署到子路径
   server: {
     port: 5173,
     open: true         // npm run dev 自动开浏览器
